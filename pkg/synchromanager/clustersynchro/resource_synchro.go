09ebc490f6e115e1faddcec87a1b9ff9b54a63a3
Iceber Gu <wei.cai-nat@daocloud.io>
2021-12-10 23:06:34 +0800

    sync resource with the last resource version

--- pkg/synchromanager/clustersynchro/resource_synchro.go
+++ pkg/synchromanager/clustersynchro/resource_synchro.go
@@ -27,9 +27,10 @@ type ResourceSynchro struct {
 	cluster         string
 	storageResource schema.GroupResource
 
-	queue         queue.EventQueue
-	listerWatcher cache.ListerWatcher
-	cache         *informer.ResourceVersionStorage
+	queue                       queue.EventQueue
+	listerWatcher               cache.ListerWatcher
+	cache                       *informer.ResourceVersionStorage
+	syncWithLastResourceVersion bool
 
 	memoryVersion schema.GroupVersion
 	convertor     runtime.ObjectConvertor
@@ -69,6 +70,9 @@ func newResourceSynchro(cluster string, lw cache.ListerWatcher, rvcache *informe
 		closed: make(chan struct{}),
 	}
 	close(synchro.stoped)
+
+	// TODO(iceber): add feature gate
+	synchro.syncWithLastResourceVersion = true
 
 	status := clustersv1alpha1.ClusterResourceSyncCondition{
 		Status:             clustersv1alpha1.SyncStatusPending,
@@ -139,7 +143,10 @@ func (synchro *ResourceSynchro) Run(stopCh <-chan struct{}) {
 		synchro.cache,
 		&unstructured.Unstructured{},
 		synchro,
-	).Run(informerStopCh)
+	).Run(synchro.syncWithLastResourceVersion, informerStopCh)
+
+	// next run informer with last resource version
+	synchro.syncWithLastResourceVersion = true
 
 	status = clustersv1alpha1.ClusterResourceSyncCondition{
 		Status:             clustersv1alpha1.SyncStatusStop,
