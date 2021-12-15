6026a3189a96c4b13efb889284098ef2226bef17
Iceber Gu <wei.cai-nat@daocloud.io>
2021-12-15 17:19:21 +0800

    bump kubernetes to v1.23.0-k3s1

--- vendor/k8s.io/component-base/version/base.go
+++ vendor/k8s.io/component-base/version/base.go
@@ -2,9 +2,9 @@ package version
 
 var (
 	gitMajor = "1"
-	gitMinor = "22"
-	gitVersion   = "v1.22.4-k3s1"
-	gitCommit    = "00226be68bd6b70f8fca252a1027fc0a35d070b8"
+	gitMinor = "23"
+	gitVersion   = "v1.23.0-k3s1"
+	gitCommit    = "a0ac6424b0847e9fcdc85c670899cfdc747e05f2"
 	gitTreeState = "clean"
-	buildDate = "2021-11-17T22:30:06Z"
+	buildDate = "2021-12-07T23:05:41Z"
 )
